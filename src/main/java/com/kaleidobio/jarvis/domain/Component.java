b3099366db2aae5567e10c6d2d70f98e4fbe5b27
mschreiber <mark.schreiber@kaleido.com>
2019-10-22 17:03:42 -0400

    improved mappings

--- src/main/java/com/kaleidobio/jarvis/domain/Component.java
+++ src/main/java/com/kaleidobio/jarvis/domain/Component.java
@@ -1,16 +1,21 @@
 package com.kaleidobio.jarvis.domain;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Map;
 
 @Data
+@Builder
+@NoArgsConstructor
 public class Component {
     private Long id;
     private String name;
     private String altName;
     private String classification;
     private String classificationSymbol;
-    private List<String> toolTip;
+    private Map<String, ?> toolTip;
     private List<String> allowedUnits;
 }
--- src/main/java/com/kaleidobio/jarvis/resource/JarvisResource.java
+++ src/main/java/com/kaleidobio/jarvis/resource/JarvisResource.java
@@ -1,10 +1,7 @@
 package com.kaleidobio.jarvis.resource;
 
 import com.kaleido.kaptureclient.client.KaptureClient;
-import com.kaleido.kaptureclient.domain.Batch;
-import com.kaleido.kaptureclient.domain.Community;
-import com.kaleido.kaptureclient.domain.Media;
-import com.kaleido.kaptureclient.domain.Supplement;
+import com.kaleido.kaptureclient.domain.*;
 import com.kaleidobio.jarvis.domain.Component;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -27,6 +25,8 @@ public class JarvisResource {
     private KaptureClient<Batch> batchKaptureClient;
     private KaptureClient<Community> communityKaptureClient;
     private KaptureClient<Supplement> supplementKaptureClient;
+
+    public static final String microLiter = "\u00B5L";
 
     public JarvisResource(KaptureClient<Media> mediaKaptureClient, KaptureClient<Batch> batchKaptureClient, KaptureClient<Community> communityKaptureClient, KaptureClient<Supplement> supplementKaptureClient) {
         this.mediaKaptureClient = mediaKaptureClient;
@@ -74,11 +74,26 @@ public class JarvisResource {
         if(batchResponse.getStatusCode().is2xxSuccessful() && batchResponse.getBody() != null){
             //map the responses to a Component
             return batchResponse.getBody().stream()
-                    .map(batch -> {
-                        Component component = new Component();
-                        //todo more mapping required maybe in a mapper class
-                        return component;
-                    })
+                    .map(batch -> Component.builder()
+                            .classification("Compound")
+                            .classificationSymbol("Cpd")
+                            .id(batch.getId())
+                            .name(batch.getName())
+                            .altName(batch.getAliases()
+                                    .stream()
+                                    .map(BatchAlias::getAlias)
+                                    .collect(Collectors.joining(",")))
+                            //todo determine what fields should be in the tool tip
+                            .toolTip(Map.of(
+                                    "Notebook", batch.getNotebook(),
+                                    "Glycan Composition", batch.getGlycanComposition(),
+                                    "Mw", batch.getMw(),
+                                    "AveDP", batch.getAveDP(),
+                                    "Concepts", batch.getConcepts().stream().map(ChemicalConcept::getConceptId)
+                                    )
+                            )
+                            .allowedUnits(List.of("%", "mL", microLiter)) //todo confirm these units
+                            .build())
                     .collect(Collectors.toList());
 
         } else {
@@ -92,11 +107,19 @@ public class JarvisResource {
         if(communityResponse.getStatusCode().is2xxSuccessful() && communityResponse.getBody() != null){
             //map the responses to a Component
             return communityResponse.getBody().stream()
-                    .map(community -> {
-                        Component component = new Component();
-                        //todo more mapping required maybe in a mapper class
-                        return component;
-                    })
+                    .map(community -> Component.builder()
+                            .classification("Community")
+                            .classificationSymbol("C")
+                            .name(community.getName())
+                            .altName(community.getAlias())
+                            .toolTip(Map.of(
+                                    "BSI Name", community.getBsiName(),
+                                    "Description", community.getDescription()
+                                    //todo more mapping??
+                            ))
+                            .allowedUnits(List.of("%", "mL", microLiter)) //todo confirm these units
+                            .build()
+                    )
                     .collect(Collectors.toList());
 
         } else {
