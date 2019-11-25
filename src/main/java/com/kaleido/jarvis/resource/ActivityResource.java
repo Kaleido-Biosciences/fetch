0d07fd786d391b827913abe4502d5d8639628efa
Pat Kyle <patrick.kyle@kaleido.com>
2019-11-25 12:58:32 -0500

    Created new endpoint to return Experiments as they are returned from Kapture

--- src/main/java/com/kaleido/jarvis/resource/ActivityResource.java
+++ src/main/java/com/kaleido/jarvis/resource/ActivityResource.java
@@ -9,10 +9,16 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/activity")
+@RequestMapping("/activities")
 @Slf4j
 public class ActivityResource {
 
@@ -22,20 +28,56 @@ public class ActivityResource {
         this.experimentKaptureClient = experimentKaptureClient;
     }
 
-    @GetMapping("/search/{searchTerm}")
-    public ResponseEntity<List<Experiment>> searchComponents(@PathVariable String searchTerm) {
-        log.debug("call to /search/{}", searchTerm);
+    @GetMapping("/experiments/search/{searchTerm}")
+    public ResponseEntity<List<Experiment>> searchExperiments(@PathVariable String searchTerm) {
+        log.debug("call to /activities/experiments/search/{}", searchTerm);
 
         final String originalSearchTerm = searchTerm;
 
-        //add asterisks to the searchTerm if they are not already there
-        if(! searchTerm.startsWith("*")){
-            searchTerm = "*"+searchTerm;
-        }
-        if(! searchTerm.endsWith("*")){
-            searchTerm = searchTerm+"*";
-        }
+        log.debug("Expanded search term to elastic search term {}", searchTerm);
+        var experimentComponents = CompletableFuture.supplyAsync(() -> searchExperiment(originalSearchTerm));
 
-        return null;
+        var matches = Stream.of(experimentComponents)
+                //wait for all the futures to complete
+                .map(CompletableFuture::join)
+                //make a combined stream of components
+                .flatMap(Collection::stream)
+                //make a boolean make with two lists, those that have an exact match and those that don't
+                .collect(Collectors.partitioningBy(experiment -> experiment.getName().equals(originalSearchTerm)));
+
+        //sort the matches that are not exact
+        List<Experiment> sortedNonExactMatches = matches.get(false).stream()
+                .sorted(Comparator.comparing(Experiment::getName))
+                .collect(Collectors.toList());
+
+        log.debug("Sorted non-exact matches {}", sortedNonExactMatches);
+
+        //don't sort exact matches
+        List<Experiment> sortedExactMatches = matches.get(true).stream().collect(Collectors.toList());
+
+        log.debug("Sorted exact matches: {}", sortedExactMatches);
+
+
+        //combine the exact and non-exact matches
+        List<Experiment> results = Stream.of(sortedExactMatches, sortedNonExactMatches)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        //return the results
+        log.debug("Combined results: {}", results);
+        return ResponseEntity.ok(results);
+    }
+
+
+    private List<Experiment> searchExperiment(String searchTerm) {
+        final var mediaResponse = experimentKaptureClient.findByFieldWithOperator("name", searchTerm, "contains");
+        if (mediaResponse.getStatusCode().is2xxSuccessful() && mediaResponse.getBody() != null) {
+            //map the responses to a Component
+            return mediaResponse.getBody().stream()
+                    .collect(Collectors.toList());
+
+        } else {
+            return Collections.emptyList();
+        }
     }
 }
