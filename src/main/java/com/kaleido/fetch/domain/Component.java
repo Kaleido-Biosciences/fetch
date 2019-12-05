76e27535306f30ec5f69e8c36b1d6a2a2be73360
Philipe Alexandre <philipe.costa@codeminer42.com>
2019-12-05 14:22:29 -0300

    Updating swagger docs

--- src/main/java/com/kaleido/fetch/domain/Component.java
+++ src/main/java/com/kaleido/fetch/domain/Component.java
@@ -1,5 +1,6 @@
 package com.kaleido.fetch.domain;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,11 +15,25 @@ import java.util.Optional;
 @AllArgsConstructor
 @Builder
 public class Component {
+
     private Long id;
+
+    @ApiModelProperty(value = "Text to display as the name of the component")
     private String name;
+
+    @ApiModelProperty(value = "Text to be shown after the Display name to give context")
     private String altName;
+
+    @ApiModelProperty(value = "The classification of the component, for example: Compound or Community or Strain")
     private String classification;
+
+    @ApiModelProperty(value = "Characters that will be shown next to the name in an icon. Components with the same classification symbol can be considered the same type of component")
     private String classificationSymbol;
+
+    @ApiModelProperty(value = "Ordered List of fields to display in a tool tip on hover of the component")
     private Map<String, Optional<?>> toolTip;
+
+    @ApiModelProperty(value = "List of units the component can be measured in")
     private List<String> allowedUnits;
+
 }
