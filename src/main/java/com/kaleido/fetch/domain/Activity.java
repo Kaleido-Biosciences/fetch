2b977e8f0fa8b95f2b1bbfbab3643e9cbce61664
Wes <wesley.fowlks@kaleido.com>
2020-01-08 13:13:18 -0500

    started implementation of save

---
+++ src/main/java/com/kaleido/fetch/domain/Activity.java
@@ -0,0 +1,33 @@
+package com.kaleido.fetch.domain;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Activity {
+    private Long id;
+
+    @ApiModelProperty(value = "The friendly name of the activity.")
+    private String name;
+
+    @ApiModelProperty(value = "Description of the activity.")
+    private String description;
+
+    @ApiModelProperty(value = "Date time of when the activity was last modified.")
+    private ZonedDateTime lastModified;
+
+    @ApiModelProperty(value = "List of IDs for versions along with timestamps of when they were completed.")
+    private List<ActivityVersion> completedVersions;
+
+    @ApiModelProperty(value = "Plate map data associated with the activity. Note: This string is currently LZW encoded as the plain text is quite large")
+    private String plateMap;
+}
--- src/main/java/com/kaleido/fetch/resource/ActivityResource.java
+++ src/main/java/com/kaleido/fetch/resource/ActivityResource.java
@@ -1,5 +1,6 @@
 package com.kaleido.fetch.resource;
 
+import com.kaleido.fetch.domain.Activity;
 import com.kaleido.fetch.service.ActivityService;
 import com.kaleido.kaptureclient.domain.Experiment;
 import io.swagger.annotations.Api;
@@ -7,10 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,4 +33,45 @@ public class ActivityResource {
         return ResponseEntity.ok(activityService.findActivities(searchTerm));
     }
 
+    @ApiOperation(value = "Retrieves the list of completed versions for the given activity id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Experiment[].class),
+            @ApiResponse(code = 400, message = "Invalid status value")
+    })
+    @GetMapping("/completed/search/{id}")
+    public ResponseEntity<List<Experiment>> getCompletedVersions(@PathVariable String id) {
+        return ResponseEntity.ok(activityService.findActivities(id));
+    }
+
+    @ApiOperation(value = "Retrieves the version of the activity for the given id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Activity.class),
+            @ApiResponse(code = 400, message = "Invalid status value")
+    })
+    @GetMapping("/completed/{id}/{version}")
+    public ResponseEntity<Activity> getActivity(@PathVariable String id, @PathVariable String version) {
+        return ResponseEntity.ok(activityService.getActivity(Long.valueOf(id)));
+    }
+
+    @ApiOperation(value = "Saves the state of the activity that is passed to it.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation, returns the most recent activity with an updated lastModified time", response = Activity.class),
+            @ApiResponse(code = 400, message = "Activity was not most recent activity, and returns the most up to date activity", response = Activity.class)
+    })
+    @PostMapping("/save")
+    public ResponseEntity<Activity> saveActivity(@RequestBody Activity activity) {
+        //TODO: Add call to the service to save the working draft of the activity.
+        return null;
+    }
+
+    @ApiOperation(value = "Saves a snapshot of the passed activity, and creates a version which can be retrieved at a later time.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successful operation", response = Activity.class),
+            @ApiResponse(code = 400, message = "Invalid status value")
+    })
+    @PostMapping("/save/completed")
+    public ResponseEntity<Activity> saveCompletedActivity(@RequestBody Activity activity) {
+        //TODO: Add the call to the service to save
+        return null;
+    }
 }
