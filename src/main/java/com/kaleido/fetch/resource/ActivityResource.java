c370bf7c49d9d6a1115a3215467859a10b96db0d
QpYujin <yujin@qpair.io>
2020-01-24 15:42:01 -0500

    adding saveActivityDraft  saves platemap info for an existing activity in cabinet. there seems to be a circular dependency between platemap and activity

--- src/main/java/com/kaleido/fetch/resource/ActivityResource.java
+++ src/main/java/com/kaleido/fetch/resource/ActivityResource.java
@@ -3,6 +3,7 @@ package com.kaleido.fetch.resource;
 import com.kaleido.fetch.domain.Activity;
 import com.kaleido.fetch.service.ActivityService;
 import com.kaleido.kaptureclient.domain.Experiment;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -52,16 +53,16 @@ public class ActivityResource {
     public ResponseEntity<Activity> getActivity(@PathVariable String id, @PathVariable String version) {
         return ResponseEntity.ok(activityService.getActivity(Long.valueOf(id)));
     }
-
+    
     @ApiOperation(value = "Saves the state of the activity that is passed to it.")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successful operation, returns the most recent activity with an updated lastModified time", response = Activity.class),
             @ApiResponse(code = 400, message = "Activity was not most recent activity, and returns the most up to date activity", response = Activity.class)
     })
     @PostMapping("/save")
-    public ResponseEntity<Activity> saveActivity(@RequestBody Activity activity) {
+    public ResponseEntity<ResponseEntity<Activity>> saveActivity(@RequestBody Activity activity) {
         //TODO: Add call to the service to save the working draft of the activity.
-        return null;
+        return ResponseEntity.ok(activityService.saveActivityDraft(activity));
     }
 
     @ApiOperation(value = "Saves a snapshot of the passed activity, and creates a version which can be retrieved at a later time.")
