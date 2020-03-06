eefc0c0939b4dfd16ee906e9388648651b9d82aa
QpYujin <yujin@qpair.io>
2020-03-06 15:14:04 -0500

    Undo the most recent commit
    Set status in fetch, and cabinet will update based on it

--- src/main/java/com/kaleido/fetch/resource/ActivityResource.java
+++ src/main/java/com/kaleido/fetch/resource/ActivityResource.java
@@ -86,7 +86,7 @@ public class ActivityResource {
     })
     @PostMapping("/save/completed")
     public ResponseEntity<ResponseEntity<String>> saveCompletedActivity(@RequestBody PlateMap plateMap) {
-        //TODO: Add the call to the service to save
+        plateMap.setStatus("COMPLETED");
         return ResponseEntity.ok(activityService.saveActivityDraft(plateMap));
     }
     
