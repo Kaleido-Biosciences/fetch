2ca2b5e4add11e3cff3522248d6b650bdb2b021a
Pat Kyle <patrick.kyle@kaleido.com>
2021-09-24 15:59:52 -0400

    add swagger profile

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java ${JAVA_OPTS} -Dspring.profiles.active=prod -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
+exec java ${JAVA_OPTS} -Dspring.profiles.active=prod,swagger -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
