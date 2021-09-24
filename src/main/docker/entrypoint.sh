a68de6f100f0e66375f1bc9b01789ea9d503e686
Pat Kyle <patrick.kyle@kaleido.com>
2021-09-24 15:54:51 -0400

    prod

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-exec java ${JAVA_OPTS} -Pprod -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
+exec java ${JAVA_OPTS} -Dspring.profiles.active=prod -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
